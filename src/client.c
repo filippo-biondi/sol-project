4d91fa0dcf68814432bd7c37159fc6c09bf7b3c2
filippo-biondi <83963482+filippo-biondi@users.noreply.github.com>
2021-06-13 18:45:34 +0200

    Update client.c

--- src/client.c
+++ src/client.c
@@ -1,40 +1,17 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
+#include <my_lib.h>
 
-#define MAX_FILENAME_LEN 256
-#define MAX_DIRNAME_LEN 256
-#define MAX_SKTNAME_LEN 256
-#define MAX_NUMERIC_STR 13
-#define MAX_FILE_NO 50
+#define MAX_FILE_NO 50 
 #define MSEC 10
 #define MAX_WAIT_TIME 10
-#define PRINT_USAGE printf("?");
-#define PRINT_OPERATION(...) if(print_operation){printf(__VA_ARGS__);}
-
-struct message
-{
-  char[MAX_FILENAME_LEN] file;
-  
-};
-
-/*struct file_name
-{
-  char* name;
-  char* dirname;
-};*/
 
 extern void execute_command(int opt, char* argv[], char* optarg);
-int write_dir(dirname, n);
-char* find_dir(char* argv[], char* optind);
 
 int print_operation = 0;
 
 int main(int argc, char* argv[])
 {
-  CHECK_ARGC(?)
+  CHECK_ARGC(2)
   
-  int msec = MSEC
   int t = 0;
   char* endptr;
   struct timespec abstime;
@@ -74,22 +51,22 @@ int main(int argc, char* argv[])
     {
       if(socket_name == NULL)
       {
-        MALLOC(socket_name, strnlen(argv[i+1], MAX_SKTNAME_LEN) + 1)
-        strncpy(socket_name, argv[i+1], MAX_SKTNAME_LEN);
-        socket_name[MAX_SKTNAME_LEN] = '\0';
+        MALLOC(socket_name, strnlen(argv[i+1], NAME_MAX) + 1)
+        strncpy(socket_name, argv[i+1], NAME_MAX);
+        socket_name[NAME_MAX] = '\0';
       }
       else
       {
         printf("Too many socket name specified\n");
         PRINT_USAGE
         return 0;
-      }
+      }message
     }
   }
   
   clock_gettime(CLOCK_REALTIME, &abstime);
   abstime.tv_sec += MAX_WAIT_TIME;
-  if(openConnection(socket_name, msec, abstime) == -1)
+  if(openConnection(socket_name, MSEC, abstime) == -1)
   {
     perror("Connection failed");
     exit(EXIT_FAILURE);
@@ -97,11 +74,12 @@ int main(int argc, char* argv[])
   PRINT_OPERATION("Connection opened on socket %s\n", socket_name)
   
   int opt;
-  opt = getopt(argc, argv, "w:W:r:R::d:D:t:l:u:c:");
+  opt = getopt(argc, argv, "w:W:r:R::d:D:l:u:c:fhtp");
   while(opt != -1)
   {
-    execute_command(opt, argv, optarg);
+    execute_command(opt, argc, argv);
     nanosleep(sleeptime, NULL);
+    opt = getopt(argc, argv, ":w:W:r:R::d:D:l:u:c:fhtp");
   }
   if(closeConnection(socket_name) == 0)
   {
