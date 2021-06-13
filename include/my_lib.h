a2ae6d25aca866b7d060bc4e94537d45ba2eeb55
filippo-biondi <83963482+filippo-biondi@users.noreply.github.com>
2021-06-13 23:51:41 +0200

    Add files via upload

--- include/my_lib.h
+++ include/my_lib.h
@@ -1,3 +1,9 @@
+#if !defined(_MY_LIB)
+#define _MY_LIB
+
+#define _POSIX_C_SOURCE 200809L
+#define _XOPEN_SOURCE 700
+
 #include <unistd.h>
 #include <assert.h>
 #include <stdio.h>
@@ -6,7 +12,17 @@
 #include <sys/wait.h>
 #include <sys/uio.h>
 #include <limits.h>
+#include <time.h>
+#include <ftw.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <errno.h>
 
+#define MAX_FILE_NO 50 
+#define MSEC 10
+#define MAX_WAIT_TIME 10
 #define MAX_OPEN_DIR 15
 
 #define PRINT_USAGE printf("Usage to be added!!!!!");
@@ -17,11 +33,11 @@ if(print_operation)          \
   printf(__VA_ARGS__);       \
 }                            \
 
-PRINT_OPERATION_ERROR(str) \
-if(print_operation)        \
-{                          \
-  perror(str);             \
-}                          \
+#define PRINT_OPERATION_ERROR(str) \
+if(print_operation)                \
+{                                  \
+  perror(str);                     \
+}                                  \
 
 #define CHECK_ARGC(n)            \
 if(argc < n)                     \
@@ -30,11 +46,13 @@ if(argc < n)                     \
   return 0;                      \
 }
                       
-#define MALLOC(ptr, bytes)\
+#define MALLOC(ptr, bytes)        \
 if((ptr = malloc(bytes)) == NULL) \
 {                                 \
   perror("Malloc error");         \
   exit(EXIT_FAILURE);             \
 }                                 \
 
-int timecmp(timespec t1, timespec t2);
+int timecmp(struct timespec t1, struct timespec t2);
+
+#endif
