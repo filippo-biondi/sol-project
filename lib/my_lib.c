09d04ac5df610c1f9ba9bc27ad82c0ef04ed0b71
filippo-biondi <83963482+filippo-biondi@users.noreply.github.com>
2021-06-13 07:41:21 +0200

    Create my_lib.c

---
+++ lib/my_lib.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/uio.h>
+
+#define CHECK_ARGC(n)\
+if(argc < n)                     \
+{                                \
+  printf("Invalid arguments\n"); \
+  return 0;                      \
+}
+                      
+#define MALLOC(ptr, bytes)\
+if((ptr = malloc(bytes)) == NULL) \
+{                                 \
+  perror("Malloc error");         \
+  exit(EXIT_FAILURE);             \
+}                                 \
